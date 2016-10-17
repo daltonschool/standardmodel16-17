09c8efc87d39932f68061783f4e4918bbe7506e7
Alex Studer <alex_studer@outlook.com>
2016-10-17 12:42:42 -0400

    lots of changes - it can now go from start to finish, but can't press the button _yet_

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOperation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOperation.java
@@ -1,23 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
-import java.util.Date;
 import android.graphics.Color;
-import android.view.Gravity;
 
 import com.qualcomm.hardware.adafruit.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-
-import java.text.SimpleDateFormat;
-
-import java.util.Locale;
 
 @Autonomous(name="Autonomous Operation")
 public class AutonomousOperation extends LinearOpMode
@@ -25,12 +20,14 @@ public class AutonomousOperation extends LinearOpMode
     private ElapsedTime runtime = new ElapsedTime();
 
     private DcMotor leftMotor = null;
-    private DcMotor leftBackMotor = null;
+    //private DcMotor leftBackMotor = null;
     private DcMotor rightMotor = null;
-    private DcMotor rightBackMotor = null;
+    //private DcMotor rightBackMotor = null;
     private IMU imu = null;
     private float distanceDriven = 0;
     private ColorSensor colorSensor = null;
+    private Servo beaconServo = null;
+    private ColorSensor beaconColor = null;
 
     private Alliance currentAlliance = Alliance.RED; // this is hardcoded for now, but should be set somehow (two different opmodes? how is this normally done?)
     private Vuforia vuforia;
@@ -42,18 +39,24 @@ public class AutonomousOperation extends LinearOpMode
 
         // motors
         leftMotor  = hardwareMap.dcMotor.get("left motor");
-        leftBackMotor  = hardwareMap.dcMotor.get("left back");
+        //leftBackMotor  = hardwareMap.dcMotor.get("left back");
         rightMotor = hardwareMap.dcMotor.get("right motor");
-        rightBackMotor = hardwareMap.dcMotor.get("right back");
+        //rightBackMotor = hardwareMap.dcMotor.get("right back");
 
         // motor directions
         leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        leftBackMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        //leftBackMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-        rightBackMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        //rightBackMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
+        // servos
+        beaconServo = hardwareMap.servo.get("beacon servo");
 
         // color sensor
-        colorSensor = hardwareMap.colorSensor.get("color sensor");
+        //colorSensor = hardwareMap.colorSensor.get("color sensor");
+        beaconColor = hardwareMap.colorSensor.get("beacon color");
+        beaconColor.setI2cAddress(I2cAddr.create8bit(0x4C));
+        beaconColor.enableLed(false);
 
         // imu
         imu = new IMU();
@@ -82,24 +85,63 @@ public class AutonomousOperation extends LinearOpMode
                 telemetry.addData("Pos", "I am lost :(");
             }
 
-            //leftMotor.setPower(0.25);
-            //rightMotor.setPower(0.25);
+            moveForward(18, 0.13f);
+            telemetry.addLine("MOVE DONE");
+            telemetry.update();
+            Thread.sleep(3000);
 
+            turnToHeading(70, 0.2f);
+            telemetry.addLine("TURN DONE");
+            telemetry.update();
+            Thread.sleep(3000);
+
+            moveForward(10, 0.13f);
+            telemetry.addLine("MOVE PART 2 DONE");
+            telemetry.update();
+            Thread.sleep(3000);
+
+            // align to gears
+            telemetry.addLine("TRYING TO ALIGN");
+            telemetry.update();
             alignTest();
+            Thread.sleep(3000);
+
+            telemetry.addLine("BEACON COLOR");
+            telemetry.update();
+            Thread.sleep(3000);
+
+            for (int i = 0; i < 1500; i++) {
+                telemetry.addData("r", beaconColor.red());
+                telemetry.addData("g", beaconColor.green());
+                telemetry.addData("b", beaconColor.blue());
+                telemetry.update();
+                Thread.sleep(1);
+            }
+
+            // determine beacon color
+            Alliance leftColor = getBeaconLeftColor();
+            if (leftColor == currentAlliance) {
+                telemetry.addLine("PRESSING LEFT");
+                beaconServo.setPosition(0.1);
+            } else {
+                telemetry.addLine("PRESSING RIGHT");
+                beaconServo.setPosition(1.0);
+            }
+            telemetry.update();
+            Thread.sleep(3000);
+
+            telemetry.addLine("THE END");
+            telemetry.update();
+
+            // press button
+            requestOpModeStop();
 
             telemetry.update();
-            //turnToHeading(0, 0.20);
-            //turnToHeading(-90, 0.20);
-            //requestOpModeStop();
-
-            /*telemetry.addData("Clear", colorSensor.alpha());
-            telemetry.addData("Red  ", colorSensor.red());
-            telemetry.addData("Green", colorSensor.green());
-            telemetry.addData("Blue ", colorSensor.blue());*/
+            idle();
         }
     }
 
-    public void alignTest() { // only works for gears!!!
+    public void alignTest() throws InterruptedException { // only works for gears!!!
         while (true) {
             if (!vuforia.hasLocation()) {
                 vuforia.update();
@@ -109,17 +151,27 @@ public class AutonomousOperation extends LinearOpMode
             Orientation orientation = vuforia.getOrientation();
             boolean orientationGood = ((orientation.thirdAngle > -160 && orientation.thirdAngle < -180) ||
                                         (orientation.thirdAngle > 160 && orientation.thirdAngle < 180));
+
+            if (translation.get(0) < -1360) {
+                // we're too close, just stop
+                telemetry.addLine("ALIGNTEST IS DONE");
+                leftMotors(0.0f);
+                rightMotors(0.0f);
+                return;
+            }
+
             if (!orientationGood && orientation.thirdAngle < 0) {
                 leftMotors(0.1f);
                 rightMotors(0.05f);
             } else if (!orientationGood && orientation.thirdAngle > 0) {
                 leftMotors(0.05f);
                 rightMotors(0.1f);
-            } else if (translation.get(0) > -900) {
+            } else if (translation.get(0) > -1360) {
                 //moveForward(Math.abs((-1100 - translation.get(0)) / 4), 0.2f);
                 leftMotors(0.1f);
                 rightMotors(0.1f);
             } else {
+                telemetry.addLine("ALIGNTEST IS DONE");
                 leftMotors(0.0f);
                 rightMotors(0.0f);
                 return;
@@ -128,19 +180,21 @@ public class AutonomousOperation extends LinearOpMode
 
             telemetry.addData("Pos", vuforia.getLocationAsString());
             telemetry.addData("Ptest", translation.get(0));
+            telemetry.addData("Still going", (translation.get(0) > -1400));
             telemetry.addData("Test", leftMotor.getCurrentPosition());
             telemetry.update();
+            idle();
         }
     }
 
     public void leftMotors(double power) {
         leftMotor.setPower(power);
-        leftBackMotor.setPower(power);
+        //leftBackMotor.setPower(power);
     }
 
     public void rightMotors(double power) {
         rightMotor.setPower(power);
-        rightBackMotor.setPower(power);
+        //rightBackMotor.setPower(power);
     }
 
     public void turnToHeading(float targetHeading, double power) {
@@ -174,19 +228,22 @@ public class AutonomousOperation extends LinearOpMode
         rightMotors(0.0);
     }
 
-    public void moveForward(double distanceToDrive, double power) {
+    public void moveForward(double distanceToDrive, double power) throws InterruptedException {
         distanceDriven = 0.0f;
         while (distanceDriven < (distanceToDrive * 100)) {
             leftMotors(power);
             rightMotors(power);
             imu.update();
+            idle();
             distanceDriven += Math.abs(imu.getGravity().xAccel);
+            telemetry.addData("distanceDriven", distanceDriven);
+            telemetry.update();
         }
         leftMotors(0.0);
         rightMotors(0.0);
     }
 
-    public void lineFollower() {
+    public void lineFollower() throws InterruptedException {
 
         // hsvValues is an array that will hold the hue, saturation, and value information.
         float hsvValues[] = {0F, 0F, 0F};
@@ -212,6 +269,7 @@ public class AutonomousOperation extends LinearOpMode
                 turnToHeading((imu.getHeading() - (i * 5)), .3);
                 while (hsvValues[2] - targetIntensity > 5 || i < 90) {
                     turnToHeading((imu.getHeading() - 5), .3);
+                    i++;
                 }
             }
 //            else
@@ -220,6 +278,20 @@ public class AutonomousOperation extends LinearOpMode
 //                break;
 //            }
         }
+    }
+
+    public Alliance getBeaconLeftColor() throws InterruptedException {
+        beaconServo.setPosition(0.45);
+        telemetry.addLine("WAITING FOR SERVO POS");
+        telemetry.update();
+        Thread.sleep(5000);
+        Alliance response;
+        if (beaconColor.red() != 255 && beaconColor.red() > 100) {
+            response = Alliance.RED;
+        } else {
+            response = Alliance.BLUE;
+        }
+        return response;
     }
 
 }
