464baafa7fe5e5502a72d81f7ad91cb0e9ea0487
c20et <c20et@dalton.org>
2016-11-21 19:10:24 -0500

    line follower

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/LineFollowingTask.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.tasks;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.Utils;
+import org.firstinspires.ftc.teamcode.taskutil.Task;
+
+/**
+ * Created by student on 11/21/16.
+ */
+public class LineFollowingTask extends Task {
+    public LineFollowingTask(Object e) {
+        super(e);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void run() throws InterruptedException {
+        while (true) {
+            // do vuforia stuff
+            if (finished) {
+                break;
+            }
+        }
+
+        while (true) {
+            // line follow
+            if (finished) {
+                break;
+            }
+        }
+    }
+}
