036343d374b415f94ecdf2d4475c9e05884e00c8
c20et <c20et@dalton.org>
2016-11-24 11:16:58 -0500

    Line following stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/LineFollowingTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/LineFollowingTask.java
@@ -1,76 +1,71 @@
 package org.firstinspires.ftc.teamcode.tasks;
 
 import android.graphics.Color;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
-
+import org.firstinspires.ftc.teamcode.Alliance;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.sensors.ColorSensors;
+import org.firstinspires.ftc.teamcode.taskutil.Task;
+import org.firstinspires.ftc.teamcode.Robot;
 import static org.firstinspires.ftc.teamcode.Robot.leftMotors;
 import static org.firstinspires.ftc.teamcode.Robot.rightMotors;
 
 /**
  * Created by student on 11/21/16.
  */
-public class LineFollowingTask extends OpMode {
-
-    public static ColorSensor lineSensor_1 = null;
-    public static ColorSensor lineSensor_2 = null;
-    double lastError = 0;
-    double rightBaseSpeed = .5;
-    double leftBaseSpeed = .5;
-    boolean nearBeacon = false; //replace with condition to check we are close to the beacon
-//    double rightMaxSpeed = 1;
-//    double leftMaxSpeed = 1;
-//    double Kp = 0;
-//    double Kd = 0;
+public class LineFollowingTask extends Task {
+    public LineFollowingTask(Object e) {
+        super(e);
+    }
+    ColorSensors obj1 = new ColorSensors();
 
     @Override
     public void init() {
-        lineSensor_1 = hardwareMap.colorSensor.get("ls 1");
-        lineSensor_2 = hardwareMap.colorSensor.get("ls 2");
+
     }
 
     @Override
-    public void loop()  {
-        while (!nearBeacon) {
-            float hsvValues_1[] = {0F,0F,0F};
-            float hsvValues_2[] = {0F,0F,0F};
-            Color.RGBToHSV((lineSensor_1.red() * 255) / 800, (lineSensor_1.green() * 255) / 800, (lineSensor_1.blue() * 255) / 800, hsvValues_1);
-            double sensorVal_1 = hsvValues_1[1];
-            double sensorVal_2 = hsvValues_2[1];
-            double whiteLineIntensityValue = 1; //Replace with value that we get
-            double blackMatIntensityValue = 0;  //Also do that here
-           // int position = qtrrc.readLine(sensors); // get calibrated readings along with the line position, refer to the QTR Sensors Arduino Library for more details on line position.
-            //double error = colorVal - targetIntensity;
+    public void run()  {
+        //to set correct right side color
+        Alliance rightColor;
+        if (Robot.beaconColor.red() == Robot.beaconColor.blue()) {
+            rightColor = Alliance.UNKNOWN;
+        } else if (Robot.beaconColor.red() > Robot.beaconColor.blue()) {
+            rightColor = Alliance.RED;
+        } else {
+            rightColor = Alliance.BLUE;
+        }
 
-            //double motorSpeed = Kp * error + Kd * (error - lastError);
-            //lastError = error;
-            if(sensorVal_1 > .5 && sensorVal_2 > .5) { //if on course
+        //actual line following code
+        while (rightColor == Alliance.UNKNOWN) {
+            //gets values of the 2 sensors
+            double sensorVal_1 = obj1.getColorSensorVal("ls1");
+            double sensorVal_2 = obj1.getColorSensorVal("ls2");
+
+            //is values of the mat and line
+            double whiteLineIntensityValue = 1.0; //REPLACE WITH CORRECT VALUE
+            double blackMatIntensityValue = 0.0;  //ALSO REPLACE HERE
+
+            //threshold of when the sensor is reading while and when it is reading black
+            double thresholdVal = (whiteLineIntensityValue + blackMatIntensityValue)/2; //REPLACE WITH DERIVED VALUE
+
+            //simple logic for line following
+            if(sensorVal_1 < thresholdVal && sensorVal_2 < thresholdVal) { //if on course (black and black)
                 rightMotors(.8);
                 leftMotors(.8);
             }
-            else if(sensorVal_1<.5 && sensorVal_2>.5){ //if needs to turn left
+            else if(sensorVal_1 > thresholdVal && sensorVal_2 < thresholdVal){ //if needs to turn left (white and black)
                 rightMotors(.8);
                 leftMotors(-.8);
             }
-            else if(sensorVal_1>.5 && sensorVal_2<.5){ //if needs to turn left
+            else if(sensorVal_1 < thresholdVal && sensorVal_2 > thresholdVal){ //if needs to turn right
                 rightMotors(-.8);
                 leftMotors(.8);
             }
             else {
-                telemetry.addData("Error", "I am lost");
-            }
-//            if (rightMotorSpeed > rightMaxSpeed ) {
-//                rightMotorSpeed = rightMaxSpeed; // prevent the motor from going beyond max speed
-//            }
-//            if (leftMotorSpeed > leftMaxSpeed ) {
-//                leftMotorSpeed = leftMaxSpeed; // prevent the motor from going beyond max speed
-//            }
-//
-//            rightMotors(rightMotorSpeed);
-//            leftMotors(leftMotorSpeed);
-            if () { //close to beacon
-                nearBeacon = true;
+                Robot.telemetry.addData("Error", "I am lost");
+                Robot.telemetry.update();
             }
         }
         rightMotors(0);
