14931566100faf82f5064262d06d8556c822e587
c20et <c20et@dalton.org>
2016-11-26 18:58:25 -0500

    PID stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDController.java
@@ -1,9 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 public class PIDController {
-    double P = .15; //increase until there is a lot of overshooting
-    double D = 0; //increase until the overshooting is reduced to an acceptable level
-    double I = 0; //increase until the final error is equal to zero.
+    double P; //increase until there is a lot of overshooting
+    double D; //increase until the overshooting is reduced to an acceptable level
+    double I; //increase until the final error is equal to zero.
     double Kp;
     double Ki;
     double Kd;
@@ -11,13 +11,22 @@ public class PIDController {
     double lastErr;
     long previous_time = System.currentTimeMillis();
 
+    public PIDController (double a, double b, double c) {
+        P = a;
+        I = b;
+        D = c;
+    }
+
     public double Step( double cError ) {
         long current_time = System.currentTimeMillis();
         double time_diff = (current_time - previous_time);
         double current_error = cError;
         total_integral += (current_error * time_diff);
         double error_difference = (current_error - lastErr) / time_diff;
-        double R = P * current_error + I * total_integral + D * error_difference;
+        Kp = current_error;
+        Ki = total_integral;
+        Kd = error_difference;
+        double R = P * Kp + I * Ki + D * Kd;
         lastErr = current_error;
         previous_time = current_time;
         if (R > 100) {
@@ -26,9 +35,10 @@ public class PIDController {
         else if (R < -100) {
             R = -100;
         }
-        Robot.telemetry.addData("Kp: ", (Kp * P));
-        Robot.telemetry.addData("Ki: ", (Ki * I));
-        Robot.telemetry.addData("Kd: ", (Kd * D));
+
+        Robot.telemetry.addData("Kp: ", (Kp));
+        Robot.telemetry.addData("Ki: ", (Ki));
+        Robot.telemetry.addData("Kd: ", (Kd));
         Robot.telemetry.addData("R: ", R);
         Robot.telemetry.addData("Current Error", current_error);
         return R;
