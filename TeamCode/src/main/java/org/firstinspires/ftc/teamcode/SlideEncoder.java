a0479cd816e998a32b5fff84c0c8d60532065ffb
Ryan Raymundo <student@ryan-raymundo.dalton.org>
2017-01-26 16:55:51 -0500

    stuff and things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideEncoder.java
@@ -4,57 +4,79 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-/**
- * Created by student on 1/25/17.
- */
-
 @TeleOp(name = "SlideEncoder", group = "TeleOp")
 public class SlideEncoder extends OpMode {
 
     DcMotor liftl;
     DcMotor liftr;
-    double powerMultiplier = 1;
-    double liftmaxheight;
-    double decellerationdampen = 0;
-    //boolean justpressed1 = false;
-    //boolean justpressed2 = false;
+    double powerMultiplier = 0.5;
+    double maxLiftHeight = 3200;
     double startPosL;
     double startPosR;
 
-    public void init(){
+    int leftState = 0;
+    int rightState = 0;
+
+    public void init() {
         liftl = hardwareMap.dcMotor.get("lifttest2");
         liftr = hardwareMap.dcMotor.get("lifttest1");
 
         startPosL = liftl.getCurrentPosition();
         startPosR = liftr.getCurrentPosition();
     }
-    public void loop(){
-//// TODO: 1/25/17 implement acceleration for encoder limiting
 
-        //code for liftl
-        if(liftl.getCurrentPosition()- startPosL < (liftmaxheight-decellerationdampen)){
-            if(gamepad1.a == true){
-                liftl.setPower(powerMultiplier);
-            }
-            else if(gamepad1.b == true){
-                liftl.setPower(-powerMultiplier);
+    public void loop() {
+        double leftEncoder = liftl.getCurrentPosition() - startPosL;
+        double rightEncoder = liftr.getCurrentPosition() - startPosR;
 
-            }
+        double leftMultiplier = 1;
+        if (leftState != 0) {
+            double leftTarget = (leftState == 1 ? maxLiftHeight : 300);
+            double leftDist = Math.abs(leftTarget - leftEncoder);
+            leftMultiplier = Math.min(leftDist * 0.005, 1);
         }
-        telemetry.addData("Left Encoder Val:", liftl.getCurrentPosition()-startPosL);
-        //code for liftr
-        if(liftr.getCurrentPosition()- startPosR < (liftmaxheight-decellerationdampen)){
-            if(gamepad1.a == true){
-                liftl.setPower(-powerMultiplier);
-            }
-            else if(gamepad1.b == true){
-                liftl.setPower(powerMultiplier);
 
-            }
+        double rightMultiplier = 1;
+        if (rightState != 0) {
+            double rightTarget = (rightState == 1 ? maxLiftHeight : 300);
+            double rightDist = Math.abs(rightTarget - ((rightEncoder) * -1));
+            rightMultiplier = Math.min(rightDist * 0.005, 1);
         }
-        telemetry.addData("Right Encoder Val:", liftr.getCurrentPosition()-startPosR);
+
+        if (gamepad1.a) {
+            leftState = 1;
+            rightState = 1;
+        } else if (gamepad1.b) {
+            leftState = -1;
+            rightState = -1;
+        }
+
+        if (leftState == 1 && leftEncoder > maxLiftHeight) {
+            leftState = 0;
+            liftl.setPower(0);
+        } else if (leftState == -1 && leftEncoder < 300) {
+            leftState = 0;
+            liftl.setPower(0);
+        }
+
+        if (rightState == 1 && rightEncoder < -maxLiftHeight) {
+            rightState = 0;
+            liftr.setPower(0);
+        } else if (rightState == -1 && rightEncoder > -300) {
+            rightState = 0;
+            liftr.setPower(0);
+        }
+
+        liftl.setPower(leftState * powerMultiplier * leftMultiplier);
+        liftr.setPower(-1 * rightState * powerMultiplier * rightMultiplier);
+
+        telemetry.addData("Left Encoder Val:", leftEncoder);
+        telemetry.addData("Right Encoder Val:", rightEncoder);
+        telemetry.addData("Left state", leftState);
+        telemetry.addData("Right state", rightState);
 
         //completion
         telemetry.update();
+
     }
 }
