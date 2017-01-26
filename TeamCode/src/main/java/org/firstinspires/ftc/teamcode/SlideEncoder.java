81ce022248516b97f382570243f862492e127ea9
Alex Studer <alex_studer@outlook.com>
2017-01-26 17:32:05 -0500

    fix lifty stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideEncoder.java
@@ -11,6 +11,7 @@ public class SlideEncoder extends OpMode {
     DcMotor liftr;
     double powerMultiplier = 0.5;
     double maxLiftHeight = 3200;
+    double stoppingDistance = 100;
     double startPosL;
     double startPosR;
 
@@ -31,16 +32,16 @@ public class SlideEncoder extends OpMode {
 
         double leftMultiplier = 1;
         if (leftState != 0) {
-            double leftTarget = (leftState == 1 ? maxLiftHeight : 300);
+            double leftTarget = (leftState == 1 ? maxLiftHeight : stoppingDistance);
             double leftDist = Math.abs(leftTarget - leftEncoder);
-            leftMultiplier = Math.min(leftDist * 0.005, 1);
+            leftMultiplier = Math.max(Math.min(leftDist * 0.001, 1), 0.6);
         }
 
         double rightMultiplier = 1;
         if (rightState != 0) {
-            double rightTarget = (rightState == 1 ? maxLiftHeight : 300);
+            double rightTarget = (rightState == 1 ? maxLiftHeight : stoppingDistance);
             double rightDist = Math.abs(rightTarget - ((rightEncoder) * -1));
-            rightMultiplier = Math.min(rightDist * 0.005, 1);
+            rightMultiplier = Math.max(Math.min(rightDist * 0.001, 1), 0.6);
         }
 
         if (gamepad1.a) {
@@ -54,7 +55,7 @@ public class SlideEncoder extends OpMode {
         if (leftState == 1 && leftEncoder > maxLiftHeight) {
             leftState = 0;
             liftl.setPower(0);
-        } else if (leftState == -1 && leftEncoder < 300) {
+        } else if (leftState == -1 && leftEncoder < stoppingDistance) {
             leftState = 0;
             liftl.setPower(0);
         }
@@ -62,13 +63,26 @@ public class SlideEncoder extends OpMode {
         if (rightState == 1 && rightEncoder < -maxLiftHeight) {
             rightState = 0;
             liftr.setPower(0);
-        } else if (rightState == -1 && rightEncoder > -300) {
+        } else if (rightState == -1 && rightEncoder > -stoppingDistance) {
             rightState = 0;
             liftr.setPower(0);
         }
 
         liftl.setPower(leftState * powerMultiplier * leftMultiplier);
         liftr.setPower(-1 * rightState * powerMultiplier * rightMultiplier);
+
+        if (leftState == 0 && gamepad1.left_stick_y > 0) {
+            liftl.setPower(0.3);
+        } else if (leftState == 0 && gamepad1.left_stick_y < 0) {
+            liftl.setPower(-0.3);
+        }
+
+        if (rightState == 0 && gamepad1.right_stick_y > 0) {
+            liftr.setPower(-0.3);
+        } else if (rightState == 0 && gamepad1.right_stick_y < 0) {
+            liftr.setPower(0.3);
+        }
+
 
         telemetry.addData("Left Encoder Val:", leftEncoder);
         telemetry.addData("Right Encoder Val:", rightEncoder);
